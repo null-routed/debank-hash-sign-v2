14ddc5655b86e5825ef9f6e8b871e21e91dda3bb
null-routed <55241343+null-routed@users.noreply.github.com>
2024-03-13 02:08:20 +0100

    fmt

--- js/example.js
+++ js/example.js
@@ -1,4 +1,8 @@
-const { generateSignature } = require('./debankSignature');
+const { generateSignature } = require("./debankSignature");
 
-const signatureData = generateSignature('GET', '/user/config', 'id=0xe8c19db00287e3536075114b2576c70773e039bd');
+const signatureData = generateSignature(
+  "GET",
+  "/user/config",
+  "id=0xe8c19db00287e3536075114b2576c70773e039bd"
+);
 console.log(signatureData);
